bdbe6367aa3a39c4df5f6241068dbce9e98f3ba2
Matheus <matheussantossilva4@outlook.com>
2024-12-31 21:06:11 -0300

    Add all chords to app component

--- harmony-trainer-app/src/app/app.component.ts
+++ harmony-trainer-app/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'harmony-trainer-app';
+  allChords = [
+    "C", "Cm", "C#", "C#m", "D", "Dm", "D#", "D#m",
+    "E", "Em", "F", "Fm", "F#", "F#m", "G", "Gm",
+    "G#", "G#m", "A", "Am", "A#", "A#m", "B", "Bm",
+  ];
 }
