a1eb51ca5b5353d678568f9e053b6b6527d94991
Matheus <matheussantossilva4@outlook.com>
2024-12-31 21:12:18 -0300

    Refactor app component to use reactive forms for harmony input

--- harmony-trainer-app/src/app/app.component.ts
+++ harmony-trainer-app/src/app/app.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'harmony-trainer-app';
   allChords = [
     "C", "Cm", "C#", "C#m", "D", "Dm", "D#", "D#m",
     "E", "Em", "F", "Fm", "F#", "F#m", "G", "Gm",
     "G#", "G#m", "A", "Am", "A#", "A#m", "B", "Bm",
   ];
+
+  harmonyForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.harmonyForm = this.fb.group({
+      I: ['D#'],
+      IIm: ['E#m'],
+      IIIm: ['F##m'],
+      IV: ['G#'],
+      V: ['A#'],
+      VIm: ['B#m'],
+      VIIm: ['C##dim']
+    });
+  }
+
+  ngOnInit(): void {
+    // Any additional setup if needed
+  }
 }
